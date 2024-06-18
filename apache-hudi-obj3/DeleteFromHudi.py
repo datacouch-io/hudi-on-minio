a9d87789c4a3af995d1bdd625bbb6d3355eaf561
Aman <training@datacouch.io>
2024-06-18 14:59:33 +0530

    update

---
+++ apache-hudi-obj3/DeleteFromHudi.py
@@ -0,0 +1,89 @@
+try:
+    from pyspark.sql.types import StructType, StructField, StringType, DateType, DoubleType
+    import os  # Operating system interface
+    import sys  # System-specific parameters and functions
+    import uuid  # Universal Unique Identifier
+    import pyspark  # Apache Spark Python API
+    import datetime  # Date and time manipulation
+    from pyspark.sql import SparkSession  # Spark SQL session
+    from pyspark import SparkConf, SparkContext  # Spark configuration and context
+    from faker import Faker  # Data generation library
+    import datetime  # (Duplicate) Date and time manipulation
+    from datetime import datetime  # Date and time class
+    import random  # Generate pseudo-random numbers
+    import pandas as pd  # Pandas library for data manipulation
+
+    print("Imports loaded ")
+
+except Exception as e:
+    print("error", e)  # Print any errors that occur during import
+
+# Constants for Hudi and Spark versions
+HUDI_VERSION = '0.14.0'
+SPARK_VERSION = '3.4'
+
+# Setting up PySpark environment variables
+SUBMIT_ARGS = f"--packages org.apache.hudi:hudi-spark{SPARK_VERSION}-bundle_2.12:{HUDI_VERSION},org.apache.hadoop:hadoop-aws:3.3.2 pyspark-shell"
+os.environ["PYSPARK_SUBMIT_ARGS"] = SUBMIT_ARGS
+os.environ['PYSPARK_PYTHON'] = sys.executable
+
+# Initialize Spark session with specific configurations
+spark = SparkSession.builder \
+    .config('spark.serializer', 'org.apache.spark.serializer.KryoSerializer') \
+    .config('spark.sql.extensions', 'org.apache.spark.sql.hudi.HoodieSparkSessionExtension') \
+    .config('className', 'org.apache.hudi') \
+    .config('spark.sql.hive.convertMetastoreParquet', 'false') \
+    .getOrCreate()
+
+# Configure Spark session to connect to a local S3-compatible service
+spark._jsc.hadoopConfiguration().set("fs.s3a.endpoint", "http://localhost:9000/")
+spark._jsc.hadoopConfiguration().set("fs.s3a.access.key", "admin")
+spark._jsc.hadoopConfiguration().set("fs.s3a.secret.key", "password")
+spark._jsc.hadoopConfiguration().set("fs.s3a.path.style.access", "true")
+spark._jsc.hadoopConfiguration().set("fs.s3a.connection.ssl.enabled", "false")
+spark._jsc.hadoopConfiguration().set(
+    "fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+spark._jsc.hadoopConfiguration().set("fs.s3a.aws.credentials.provider",
+                                     "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider")
+
+spark.sparkContext.setLogLevel("ERROR")
+
+
+schema = StructType([
+    StructField("order_id", StringType(), True),
+    StructField("order_date", StringType(), True),
+    StructField("order_time", StringType(), True),
+    StructField("customer_id", StringType(), True),
+    StructField("total_amount", StringType(), True)
+])
+
+db_name = "default"
+InventoryPath = f"s3a://global-emart/hudi/database={db_name}/table_name=Inventory"
+Order_ItemPath = f"s3a://global-emart/hudi/database={db_name}/table_name=Order_Item"
+OrderPath = f"s3a://global-emart/hudi/database={db_name}/table_name=Order"
+PaymentPath = f"s3a://global-emart/hudi/database={db_name}/table_name=Payment"
+ProductPath = f"s3a://global-emart/hudi/database={db_name}/table_name=Product"
+ShipmentPath = f"s3a://global-emart/hudi/database={db_name}/table_name=Shipment"
+UserPath = f"s3a://global-emart/hudi/database={db_name}/table_name=User"
+
+# Create DataFrame with the explicit schema
+# Data for the canceled order (only record keys needed)
+data = [
+    ('1', '2023-08-19', '20:35:04', '1210', '3565.53')
+]
+
+columns = ["order_id", "order_date",
+           "order_time", "customer_id", "total_amount"]
+order_df = spark.createDataFrame(data, columns)
+
+# Hudi configuration
+hudi_options = {
+    'hoodie.table.name': 'Order',
+    'hoodie.datasource.write.recordkey.field': 'order_id',
+    'hoodie.datasource.write.precombine.field': 'order_date',
+    'hoodie.datasource.write.operation': 'delete',
+}
+
+# Write to Hudi table
+order_df.write.format("hudi").options(
+    **hudi_options).mode("append").save(OrderPath)
---
+++ apache-hudi-obj3/UpsertToHudi.py
@@ -0,0 +1,113 @@
+try:
+    import os  # Operating system interface
+    import sys  # System-specific parameters and functions
+    import uuid  # Universal Unique Identifier
+    import pyspark  # Apache Spark Python API
+    import datetime  # Date and time manipulation
+    from pyspark.sql import SparkSession  # Spark SQL session
+    from pyspark import SparkConf, SparkContext  # Spark configuration and context
+    from faker import Faker  # Data generation library
+    import datetime  # (Duplicate) Date and time manipulation
+    from datetime import datetime  # Date and time class
+    import random  # Generate pseudo-random numbers
+    import pandas as pd  # Pandas library for data manipulation
+
+    print("Imports loaded ")
+
+except Exception as e:
+    print("error", e)  # Print any errors that occur during import
+
+# Constants for Hudi and Spark versions
+HUDI_VERSION = '0.14.0'
+SPARK_VERSION = '3.4'
+
+# Setting up PySpark environment variables
+SUBMIT_ARGS = f"--packages org.apache.hudi:hudi-spark{SPARK_VERSION}-bundle_2.12:{HUDI_VERSION},org.apache.hadoop:hadoop-aws:3.3.2 pyspark-shell"
+os.environ["PYSPARK_SUBMIT_ARGS"] = SUBMIT_ARGS
+os.environ['PYSPARK_PYTHON'] = sys.executable
+
+# Initialize Spark session with specific configurations
+spark = SparkSession.builder \
+    .config('spark.serializer', 'org.apache.spark.serializer.KryoSerializer') \
+    .config('spark.sql.extensions', 'org.apache.spark.sql.hudi.HoodieSparkSessionExtension') \
+    .config('className', 'org.apache.hudi') \
+    .config('spark.sql.hive.convertMetastoreParquet', 'false') \
+    .getOrCreate()
+
+# Configure Spark session to connect to a local S3-compatible service
+spark._jsc.hadoopConfiguration().set("fs.s3a.endpoint", "http://localhost:9000/")
+spark._jsc.hadoopConfiguration().set("fs.s3a.access.key", "admin")
+spark._jsc.hadoopConfiguration().set("fs.s3a.secret.key", "password")
+spark._jsc.hadoopConfiguration().set("fs.s3a.path.style.access", "true")
+spark._jsc.hadoopConfiguration().set("fs.s3a.connection.ssl.enabled", "false")
+spark._jsc.hadoopConfiguration().set(
+    "fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+spark._jsc.hadoopConfiguration().set("fs.s3a.aws.credentials.provider",
+                                     "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider")
+
+spark.sparkContext.setLogLevel("ERROR")
+
+db_name = "default"
+InventoryPath = f"s3a://global-emart/hudi/database={db_name}/table_name=Inventory"
+Order_ItemPath = f"s3a://global-emart/hudi/database={db_name}/table_name=Order_Item"
+OrderPath = f"s3a://global-emart/hudi/database={db_name}/table_name=Order"
+PaymentPath = f"s3a://global-emart/hudi/database={db_name}/table_name=Payment"
+ProductPath = f"s3a://global-emart/hudi/database={db_name}/table_name=Product"
+ShipmentPath = f"s3a://global-emart/hudi/database={db_name}/table_name=Shipment"
+UserPath = f"s3a://global-emart/hudi/database={db_name}/table_name=User"
+
+# Data for the updated product
+data = [
+    ('1', 'Wireless Headphones', 'Noise-cancelling wireless headphones',
+     'Electronics', '140.00', '1003', '2024-05-28')
+]
+
+columns = ["product_id", "name", "description",
+           "category", "price", "seller_id", "listing_date"]
+product_df = spark.createDataFrame(data, columns)
+product_df.printSchema()
+
+
+def upsert_to_hudi(spark_df,
+                   table_name,
+                   db_name,
+                   method='upsert',
+                   table_type='COPY_ON_WRITE'
+                   ):
+    path = f"s3a://global-emart/hudi/database={db_name}/table_name={table_name}"
+
+    hudi_options = {
+        'hoodie.table.name': table_name,
+        'hoodie.datasource.write.table.type': table_type,
+        'hoodie.datasource.write.table.name': table_name,
+        'hoodie.datasource.write.operation': method,
+        'hoodie.datasource.write.precombine.field': 'ts',
+        'hoodie.datasource.write.recordkey.field': 'product_id',
+
+        "hoodie.datasource.hive_sync.database": db_name,
+        "hoodie.datasource.hive_sync.table": table_name,
+        "hoodie.datasource.hive_sync.metastore.uris": "thrift://localhost:9083",
+        "hoodie.datasource.hive_sync.mode": "hms",
+        "hoodie.datasource.hive_sync.enable": "true",
+    }
+
+    print("\n")
+    print(path)
+    print("\n")
+
+    spark_df.write.format("hudi"). \
+        options(**hudi_options). \
+        mode("append"). \
+        save(path)
+
+
+try:
+    upsert_to_hudi(
+        spark_df=product_df,
+        db_name="default",
+        table_name="Product"
+    )
+    print("Upserted")
+
+except Exception as e:
+    print("error", e)
